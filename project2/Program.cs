dbf9b5aa99e086603d8de02ad890d930eef31f60
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-03 13:26:03 +0300

    project 2

--- project2/Program.cs
+++ project2/Program.cs
@@ -1,16 +1,66 @@
 ﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 
-int [] Array (int [,] Array ){
-double [] Average2dArray(int [,] array){
-    int [] Array = new int [array.GetLength(0)];
-    for(int i = 0; i<array.GetLength(0); i++){
-        int count = 0;
-        for(int j = 0; j<array.GetLength(1); j++){
-            count+=array[i,j];
-        }
-        Array[i] = count/array.GetLength(0);
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем размер массива
+        int arraySize = 10; // Можете изменить размер массива по вашему усмотрению
+
+        // Создаем массив случайных трехзначных чисел
+        int[] randomNumbers = GenerateRandomNumbers(arraySize);
+
+        // Выводим массив
+        Console.WriteLine("Сгенерированный массив:");
+        PrintArray(randomNumbers);
+
+        // Получаем количество четных чисел
+        int evenCount = CountEvenNumbers(randomNumbers);
+
+        // Выводим результат
+        Console.WriteLine($"Количество четных чисел в массиве: {evenCount}");
     }
-    return Array;
-}
+
+    // Метод для генерации случайных трехзначных чисел
+    static int[] GenerateRandomNumbers(int size)
+    {
+        Random random = new Random();
+        int[] numbers = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            numbers[i] = random.Next(100, 1000);
+        }
+
+        return numbers;
+    }
+
+    // Метод для вывода массива
+    static void PrintArray(int[] array)
+    {
+        foreach (var number in array)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+
+    // Метод для подсчета четных чисел в массиве
+    static int CountEvenNumbers(int[] array)
+    {
+        int evenCount = 0;
+
+        foreach (var number in array)
+        {
+            if (number % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+
+        return evenCount;
+    }
 }
--- project2/obj/Debug/net8.0/project2.AssemblyInfo.cs
+++ project2/obj/Debug/net8.0/project2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+216a1f98c57d194033d9a75f48f0afb0724bbcfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("project2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
